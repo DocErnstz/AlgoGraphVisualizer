d9a9d8d5e5ded276bd9053eaf283879498d11b28
DocErnstz <ernesto800@outlook.com>
2021-06-20 22:53:06 -0300

    select cell to paint

--- src/App/index.js
+++ src/App/index.js
@@ -1,1 +1,11 @@
+require("./styles.css");
+
+var elements=document.getElementById('matrix').children;
+
+function select(x,y){
+    elements.item(x).children.item(y).classList.add("red");
+}
+
+
+
 
