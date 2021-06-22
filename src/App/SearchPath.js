621cd6f780f382e45ea97c4813f3af3819ce3e61
DocErnstz <ernesto800@outlook.com>
2021-06-22 13:08:43 -0300

    drag and drop start_point

--- src/App/SearchPath.js
+++ src/App/SearchPath.js
@@ -3,7 +3,24 @@ const {Cell} = require("./DataStructure.js");
 var elements=document.getElementById('matrix').children;
 
 function select(x,y){
-    elements.item(x).children.item(y).classList.add("red");
+    const red = document.createElement("div");
+    red.classList.add("red");
+    elements.item(y).children.item(x).append(red);
+}
+
+export function Clear()
+{
+    for(var x = 0; x < 17; x++)
+    {
+        for(var y = 0; y < 17; y++)
+        {
+            //elements.item(y).children.item(x).removeChild(elements.item(x).children.item(y).firstChild);   
+            console.log(elements.item(y).children.item(x).firstChild.getElementById("red"));
+            while (elements.item(y).children.item(x).lastElementChild) {
+                elements.item(y).children.item(x).removeChild(elements.item(y).children.item(x).lastElementChild);
+              }
+        }
+    }
 }
 
 function UCS(start, goal){
@@ -13,6 +30,7 @@ function UCS(start, goal){
     var came_from = {};
     came_from[start.id] = null;
     cost_so_far[start.id] = 0;
+
     while (!frontier.isEmpty()){
         var current = frontier.front();
         frontier.dequeue();
@@ -37,7 +55,7 @@ function UCS(start, goal){
 }
 
 
-export default function pathfinding(start, goal)
+export function pathfinding(start, goal)
 {
     var current = goal.id;
     var path = []
--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,60 @@
-require("./styles.css");
-import pathfinding from "./SearchPath.js";
+require("./css/index.css");
+import {pathfinding, Clear} from "./SearchPath.js";
+
 const {Cell} = require("./DataStructure.js"); 
-console.log(pathfinding(new Cell(0,0), new Cell(2,2)));
+const start = document.getElementById("start");
+const cells = document.querySelectorAll(".col");
+var start_point = new Cell(0,0);
+
+document.getElementById("Visualizer").addEventListener("click", (e) => {
+    pathfinding(start_point, new Cell(2,2));
+});
+document.getElementById("Clear").addEventListener("click", (e) => {
+    Clear();
+});
+
+//start.addEventListener('dragstart', dragStart);
+start.addEventListener('dragend', dragEnd);
+
+for(const cell of cells){
+    cell.addEventListener("dragover", dragOver);
+    cell.addEventListener("dragenter", dragenter);
+    cell.addEventListener("dragleave", dragleave);
+    cell.addEventListener("drop", dragDrop);
+}
 
 
+
+  
+  function dragEnd() {
+    this.className = 'start';
+  }
+  
+  function dragOver(e) {
+    e.preventDefault();
+  }
+  
+  function dragenter(e) {
+    e.preventDefault();
+  }
+  
+  function dragleave() {
+    this.className = 'col';
+  }
+  
+  function dragDrop() {
+    this.className = "col";
+    this.append(start);
+    start_point = new Cell(Array.from(this.parentNode.children).indexOf(this), Array.from(this.parentNode.parentNode.children).indexOf(this.parentNode));
+  }
+
+  function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+  async function loop()
+  {
+      await timeout(3000);
+      console.log("b");
+  }
+  loop();
