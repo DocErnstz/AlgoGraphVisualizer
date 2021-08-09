fefdc03e1c2cccdf6ab41d5986deef38e9ad037d
DocErnstz <ernesto800@outlook.com>
2021-08-09 14:12:54 -0300

    click col

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,6 @@ import "@testing-library/jest-dom/extend-expect";
 import { JSDOM } from "jsdom";
 import fs from "fs";
 import path from "path";
-import { block_setter } from "./App/utils";
 
 const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
 
@@ -25,17 +24,7 @@ describe("index.html", () => {
     const colsArr = Array.from(cols);
     const colsArrF = colsArr.filter((x) => colsArr.indexOf(x) > 1);
     expect(colsArrF.length).toBe(322);
-    expect(document.getElementsByClassName("col")).toBe([]);
-  });
-  it("block works out", () => {
-    const scol = getByText(container, "a");
-    const block_point = block_setter(scol);
-    expect(scol).toHaveClass("block");
-    const blockobject = {
-      x: block_point.x,
-      y: block_point.y,
-      id: block_point.id,
-    };
-    expect(blockobject).toMatchSnapshot();
+    const col = cols[4];
+    fireEvent.click(col);
   });
 });
